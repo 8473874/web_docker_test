f0dd4b45a3d344d7b01d3e2f7f32567809a400f3
Zero8473874 <8473874@qq.com>
2017-03-03 17:49:00 +0800

    更新结构

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # web_docker_test
-用docker做web容器测试
+用docker做web容器测试-代码见分支
--- app/routes/version.js
+++
@@ -1,9 +0,0 @@
-const router = require('koa-router')();
-const config = require('../config/config');
-
-router.get('/', async function(ctx, next) {
-  ctx.status = 200;
-  ctx.body = config.version;
-});
-
-module.exports = router;
